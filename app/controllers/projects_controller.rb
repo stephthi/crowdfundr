ca087af697ca19ad2a5ff02fefa843dd64bd61a5
Lauren <lemherdman@hotmail.com>
2015-12-08 16:51:30 -0500

    Create project show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,15 +2,23 @@ class ProjectsController < ApplicationController
 	def index
 		@projects = Project.all
 	end
+
 	def new
 		@project = Project.new
 	end
+
   def create
     @project = Project.new(project_params)
     if @project.save
       redirect_to projects_url
     end
   end
+
+  def show
+  	@project = Project.find(params[:id])
+  	@comment = Comment.new(user: current_user, project: @project)
+  end
+
 	private
 	def project_params
 		params.require(:project).permit(:title, :description, :funding_target, :start_date, :end_date)

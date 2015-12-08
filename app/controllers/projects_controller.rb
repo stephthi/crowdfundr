b6d8b1e69aa0d552a369ff20413733f34cfd6316
DewaldoDev <dewaldodev@gmail.com>
2015-12-08 16:56:36 -0500

    Created index page and project partial

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,16 +1,21 @@
 class ProjectsController < ApplicationController
+
 	def index
 		@projects = Project.all
 	end
+
 	def new
 		@project = Project.new
 	end
+
   def create
     @project = Project.new(project_params)
+
     if @project.save
       redirect_to projects_url
     end
   end
+
 	private
 	def project_params
 		params.require(:project).permit(:title, :description, :funding_target, :start_date, :end_date)

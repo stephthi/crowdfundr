1460175947f75e0a7cad96fc455ffccbd2f737c8
stephthi <nstephanie2@gmail.com>
2015-12-08 16:36:57 -0500

    Added methods to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+	before_action :ensure_logged_in, only:[:show]
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to projects_url, notice: "Signed Up!"
+		else
+			render "new"
+		end
+	end
+
+	def show
+		@user = current_user
+	end
+
+	private
+	def user_params
+			params.require(:user).permit(:username, :email, :password, :password_confirmation)
+	end
 end

c52b5475de7762d7fcd4749348af011c1cbd2a81
DewaldoDev <dewaldodev@gmail.com>
2015-12-09 14:57:34 -0500

    Created set_reward method in Pledge model

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -5,4 +5,16 @@ class Pledge < ActiveRecord::Base
 
 	validates :dollar_amount, presence: true
 	validates :dollar_amount, numericality: {greater_than: 0}
+
+	def set_reward
+		rewards = self.project.rewards
+		rewards.each do |reward|
+			if self.dollar_amount >= reward.min_dollar_amount && reward.quantity > 0
+				self.reward = reward
+			else
+				return
+			end
+		end
+	end
+
 end

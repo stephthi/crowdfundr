75c06d8d6fb8156bed6af2a16822ca24b5ac84c0
stephthi <nstephanie2@gmail.com>
2015-12-09 14:15:23 -0500

    Fixed extra bracket in sessions controller, created new.html.erb in sessions and added routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:email])
 
 		if user && user.authenticate(params[:password])
-			session[:user_id)] = user.id
+			session[:user_id] = user.id
 			redirect_to projects_url, notice: "Logged In!"
 		else
 			flash.now[:alert] = "Invalid Email or Password."

5d0c58ababf91b4b1f032779fd4415a0d1fcfbce
stephthi <nstephanie2@gmail.com>
2015-12-09 17:56:26 -0500

    Added create method to pledges controller

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,2 +1,21 @@
 class PledgesController < ApplicationController
+
+	def create
+		@pledge = @project.pledges.build(pledge_params)
+		@pledge.user = current_user
+
+		respond_to do |format|
+			if @pledge.save
+				format.html { redirect_to project_path(@project_id), notice: 'Pledge added.'}
+				format.js {}
+			else
+				format.html { render 'projects/show', alert: 'There was an error.' }
+			end
+		end
+	end
+
+	private
+	def pledge_params
+		params.require(:pledge).permit(:dollar_amount, :project_id)
+	end
 end

20e3834d2fe70bfb9a2c6d4f9cf8b238f7a050b0
stephthi <nstephanie2@gmail.com>
2015-12-09 14:10:27 -0500

    Created sessions controller and create function

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+	def create
+		user = User.find_by(email: params[:email])
+
+		if user && user.authenticate(params[:password])
+			session[:user_id)] = user.id
+			redirect_to projects_url, notice: "Logged In!"
+		else
+			flash.now[:alert] = "Invalid Email or Password."
+			render "new"
+		end
+	end
+
+end

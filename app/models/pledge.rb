eff9802c0b57a26ae652a6160297aa7793918e5e
DewaldoDev <dewaldodev@gmail.com>
2015-12-10 16:04:38 -0500

    set reward method for pledge working

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -6,10 +6,16 @@ class Pledge < ActiveRecord::Base
 	validates :dollar_amount, presence: true
 	validates :dollar_amount, numericality: {greater_than: 0}
 
+	#make sure the loop goes through the lowest minimum amount reward to the highest
+	#select and sort call
+
+	#the loop does not stop if it hits a 0 reward.quantity
+	#
+
 	def set_reward
-		rewards = self.project.rewards
+		rewards = self.project.rewards.select{|reward| reward.quantity > 0 }.sort_by(&:min_dollar_amount)
 		rewards.each do |reward|
-			if self.dollar_amount >= reward.min_dollar_amount && reward.quantity > 0
+			if self.dollar_amount >= reward.min_dollar_amount
 				self.reward = reward
 			else
 				return

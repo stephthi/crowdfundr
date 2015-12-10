27fae79bc791f4a40eea018c49471b07f8d28225
stephthi <nstephanie2@gmail.com>
2015-12-10 14:39:17 -0500

    Fixed pledge user_id

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -5,27 +5,15 @@ class PledgesController < ApplicationController
 
 	def create
     @pledge = Pledge.new(pledge_params)
-    @pledge.user = current_user
+    @pledge.user_id = current_user.id
+
 
     if @pledge.save
-      redirect_to project_path(@pledge.project), notice: "Thank you for the cool pledge"
+      redirect_to project_path(@pledge.project), notice: "Thank you for the generous pledge."
     else
       render new_pledge_path, notice: 'Something went wrong!'
     end
   end
-	# def create
-	# 	@pledge = @project.pledges.build(pledge_params)
-	# 	@pledge.user = current_user
-
-	# 	respond_to do |format|
-	# 		if @pledge.save
-	# 			format.html { redirect_to project_path(@project_id), notice: 'Pledge added.'}
-	# 			format.js {}
-	# 		else
-	# 			format.html { render 'projects/show', alert: 'There was an error.' }
-	# 		end
-	# 	end
-	# end
 
 	def new
 		@pledge = Pledge.new
